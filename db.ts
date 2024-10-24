f9d31f939d11b970f98231937352a49e8acb360c
oscarheimdahl <o.heimdahl@gmail.com>
2024-10-24 22:13:43 +0200

    Move to deno slite lib

--- db.ts
+++ db.ts
@@ -1,34 +1,30 @@
 import { champions } from './data/champions.ts';
 
-import sqlite3 from 'npm:sqlite3';
+// import sqlite3 from 'npm:sqlite3';
+// const db = new sqlite3.Database('index.db');
+import { Database } from 'jsr:@db/sqlite@0.12';
+const db = new Database('index.db');
 
-const db = new sqlite3.Database('index.db');
 let dbInitialized = false;
 
-export async function initDB() {
+export function initDB() {
   if (dbInitialized) return;
   dbInitialized = true;
 
-  await createChampionsTable();
-  await seedChampions();
+  createChampionsTable();
+  seedChampions();
 }
 
 function createChampionsTable() {
-  return new Promise<void>((resolve, reject) => {
-    db.run(
-      `
+  db.run(
+    `
       CREATE TABLE IF NOT EXISTS champions (
         id TEXT PRIMARY KEY UNIQUE, 
         name TEXT UNIQUE, 
         votes INTEGER DEFAULT 0
       )
-    `,
-      (err) => {
-        if (err) reject(err);
-        resolve();
-      }
-    );
-  });
+    `
+  );
 }
 
 function seedChampions() {
@@ -38,40 +34,17 @@ function seedChampions() {
     ON CONFLICT (id) DO NOTHING
   `;
 
-  return new Promise<void>((resolve, reject) => {
-    const stmt = db.prepare(query);
+  const stmt = db.prepare(query);
 
-    champions.forEach((champion) => {
-      stmt.run([champion.id, champion.name], (err) => {
-        if (err) reject(err);
-      });
-    });
-
-    stmt.finalize((err) => {
-      if (err) reject(err);
-      resolve();
-    });
+  champions.forEach((champion) => {
+    stmt.run(champion.id, champion.name);
   });
 }
 
 export function getChampions() {
-  return new Promise((resolve, reject) => {
-    db.all(`SELECT * FROM champions ORDER BY votes DESC`, (err, rows) => {
-      if (err) reject(err);
-      resolve(rows);
-    });
-  });
+  return db.prepare(`SELECT * FROM champions ORDER BY votes DESC`).all();
 }
 
 export function upvoteChampion(championId: string) {
-  return new Promise<void>((resolve, reject) => {
-    db.run(
-      `UPDATE champions SET votes = votes + 1 WHERE id = ?`,
-      [championId],
-      (err) => {
-        if (err) reject(err);
-        resolve();
-      }
-    );
-  });
+  db.run(`UPDATE champions SET votes = votes + 1 WHERE id = ?`, championId);
 }
